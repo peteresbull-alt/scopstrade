d8c2b50e683ebaede6dd7918e311f423aaf04e88
ghostpete <ghostcoderme@gmail.com>
2026-02-11 04:05:13 +0100

    added proxies

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_APP_BACKEND_URL || "http://localhost:8000/api/auth";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_APP_BACKEND_URL || "http://127.0.0.1:8000/api/auth";
 
